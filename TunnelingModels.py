70aed849245f100e0eb9324e01b9e555ad57b35e
Julian <julian.dlugosch@tum.de>
2020-10-11 19:47:27 +0200

    added a function to load data from csv

--- TunnelingModels.py
+++ TunnelingModels.py
@@ -9,13 +9,11 @@ The models are first discribed via function and then implemented as sub-classes 
 from lmfit import Model
 from lmfit import CompositeModel
 import lmfit
-
-#from mpmath import *
-
+import pandas as pd
 from scipy import constants
-
 import numpy as np
 from numpy import exp, sqrt, sinh
+
 
 #global definition of physical constants
 hbar = constants.hbar
@@ -25,7 +23,7 @@ e = constants.elementary_charge
 pi = constants.pi
 
 
-def Simmons(v, area, alpha, phi, d, weight=1, beta=1, J=False):
+def Simmons(v, area, alpha, phi, d, weight=1, beta=1, J=False, absolute=True):
 
     d = d * 10 ** (-9)
 
@@ -36,7 +34,8 @@ def Simmons(v, area, alpha, phi, d, weight=1, beta=1, J=False):
     A = 4 * pi * beta * d * sqrt(2 * m_e) / h
 
     I = - weight * area * J_0 * (e*phi *exp(-A * alpha * sqrt(e*phi)) - (e*phi + e * v)*exp(-A * alpha * sqrt(e*phi) + e * v))
-
+    if absolute:
+        I = abs(I)
 
     return I
 
@@ -76,6 +75,34 @@ class GruvermanModel(Model):
 
 
 # Utility Functions
+
+def DataFromCSV(filename, sep, current_start_column, min_voltage, max_voltage, voltage_column=0, comments='#'):
+    """
+    This function loads current and voltage values from a csv File and returns the current as np array and
+    the currents as array of np arrays. Lines that start with the "comments" character are ignored,
+    the first line is assumed to be the column name and is also disregarded.
+    :param filename: name and location of the file where the data is stored
+    :param sep: csv seperator
+    :param voltage_column: column number in which the voltage data is stored (first column: 0)
+    :param current_start_column: first column in which current data is stored
+    :return: volatage, currents
+    """
+    dataFromFile = pd.read_csv(filename, sep=sep, comment=comments)
+    dataFromFile = dataFromFile.dropna()
+    dataFromFile = dataFromFile.reset_index(drop=True)
+
+    indicesToDrop = dataFromFile[(abs(dataFromFile.iloc[:, voltage_column].values) > max_voltage)].index
+    indicesToDrop.append(dataFromFile[(abs(dataFromFile.iloc[:, voltage_column].values)<min_voltage)].index)
+    new = dataFromFile[(abs(dataFromFile.iloc[:, voltage_column].values) < min_voltage)].index
+    dataFromFile.drop(indicesToDrop, inplace=True)
+    dataFromFile.drop(new, inplace=True)
+    print(dataFromFile.iloc[:, voltage_column].values)
+
+    voltage = dataFromFile.iloc[:, voltage_column].values
+    currents = []
+    [currents.append(dataFromFile.iloc[:, n].values) for n in range(current_start_column, len(dataFromFile.columns))]
+    return voltage, currents
+
 
 def combineSameModel(A,B):
     if A.name == B.name:
--- main.py
+++ main.py
@@ -3,41 +3,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from lmfit import Parameter
 
-#good fit fuctions so far: basinhopping, cobyla, dual_annealing
-v = np.linspace(-1, 1, 101)
+#good fit fuctions so far: basinhopping, cobyla, dual_annealing, nelder, ampgo
+v = np.linspace(-1, 1, 146)
 
 np.random.seed(123)
-noise = np.random.normal(size=len(v), scale=8e-10)
+noise = np.random.normal(size=len(v), scale=10e-10)
 
 SimmonsBarrier = SimmonsModel()
-true_params = SimmonsBarrier.make_params(area=2.5e-9, d=1.2, alpha=0.8, phi=3, weight=1)
-true = SimmonsBarrier.eval(v=v, area=2.5e-9, alpha=1, phi=3, d=1.5, weight=1, beta=1)
+#true_params = SimmonsBarrier.make_params(area=2.5e-9, d=1.2, alpha=0.8, phi=3, weight=1)
+#true = SimmonsBarrier.eval(v=v, area=2.5e-9, alpha=1, phi=3, d=1.5, weight=1, beta=1)
 data = SimmonsBarrier.eval(v=v, area=2.5e-9, alpha=1, phi=3, d=1.5, weight=1, beta=1)+noise
 
-v2= np.linspace(-0.5,0,5,101)
-print(data)
-# add with tuples: (NAME VALUE VARY MIN  MAX  EXPR  BRUTE_STEP)
-fit = SimmonsBarrier.fit(data,v=v,
-                         area=Parameter('area', value=2.5e-9, vary=False),
+
+### get data from file
+voltage, currents = DataFromCSV('testdata.csv', sep='\t', min_voltage=0.01, max_voltage=0.51, current_start_column=1, voltage_column=0)
+current = currents[1]
+fit = SimmonsBarrier.fit(current, v=voltage,
+                         area=Parameter('area', value=2.25e-10, vary=False),
                          alpha=Parameter('alpha', value=1, min=0, max=1),
-                         phi=Parameter('phi', value=3.2, min=2.5, max=3.5),
-                         d=Parameter('d', value=1, min=0.5, max=2),
-                         weight=Parameter('weight', value=1, vary=False),
-                         beta=Parameter('beta', value=1, min=0.01, max=1), method='basinhopping'
+                         phi=Parameter('phi', value=2, min=1, max=8),
+                         d=Parameter('d', value=1, min=0.5, max=3),
+                         weight=Parameter('weight', value=1, min=0.01, max=1, vary=True),
+                         beta=Parameter('beta', value=1, min=0.01, max=1, vary=True),
+                         absolute=Parameter('absolute', value=True),
+                         method='basinhopping',
                          )
 
 print(fit.fit_report())
 
 plt.figure()
-plt.plot(v, np.abs(data), 'ro')
-plt.plot(v, np.abs(fit.init_fit), label='intial fit')
-plt.plot(v, np.abs(fit.best_fit), label='best fit')
-
+#plt.plot(v, np.abs(data), 'ro')
+plt.plot(voltage, np.abs(fit.init_fit), label='intial fit')
+plt.plot(voltage, np.abs(fit.best_fit),'bo', label='best fit')
+plt.plot(voltage, current, 'ro', label='data')
 plt.legend(loc='best')
 plt.yscale('log')
+#plt.xlim(-0.5, 0.5)
 
 plt.ylabel('I(A)')
 plt.xlabel('V(V)')
-plt.title('simulated measurement')
+plt.title('best fit')
 plt.show()
 
